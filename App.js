cd99202f91b5451e610e4f8903c408c04e016045
Shubham Anand Jaiswal <jaiswalshubham502@gmail.com>
2024-09-18 19:17:58 +0530

    add drawer navigation

--- App.js
+++ App.js
@@ -1,15 +1,16 @@
+import 'react-native-gesture-handler';
+import 'react-native-reanimated'
 import { View, StyleSheet } from "react-native";
 import * as React from "react";
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import LittleLemonHeader from "./components/LittleLemonHeader";
 import LittleLemonFooter from "./components/LittleLemonFooter";
 import MenuItems from "./components/MenuItems";
 import WelcomeScreen from "./WelcomeScreen";
 import LoginScreen from './LoginScreen';
-import Ionicons from '@expo/vector-icons/Ionicons';
 
-const Tab = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
 
 function App() {
 	return (
@@ -17,24 +18,10 @@ function App() {
 			<NavigationContainer>
 				<View style={styles.container}>
 					<LittleLemonHeader />
-					<Tab.Navigator
-						screenOptions={({ route }) => ({
-							tabBarIcon: ({ focused, color, size }) => {
-								let iconName;
-								if (route.name === 'Welcome') {
-									iconName = focused ? 'home-sharp' : 'home-outline'
-								} else if (route.name === 'Login') {
-									iconName = 'enter'
-								}
-								return <Ionicons name={iconName} size={size} color={color} />
-							},
-							tabBarActiveTintColor: 'tomato',
-							tabBarInactiveTintColor: 'grey'
-						})}
-						initialRouteName="Login">
-						<Tab.Screen name="Welcome" options={{ title: 'Home', headerTitleAlign: 'center' }} component={WelcomeScreen} />
-						<Tab.Screen name="Login" options={{ title: 'Login', headerTitleAlign: 'center' }} component={LoginScreen} />
-					</Tab.Navigator>
+					<Drawer.Navigator initialRouteName="Login">
+						<Drawer.Screen name="Welcome" options={{ title: 'Home', headerTitleAlign: 'center' }} component={WelcomeScreen} />
+						<Drawer.Screen name="Login" options={{ title: 'Login', headerTitleAlign: 'center' }} component={LoginScreen} />
+					</Drawer.Navigator>
 				</View>
 				<View style={styles.footerContainer}>
 					<LittleLemonFooter />
